afe8f058aaba5019448bcfd01c42dc291fbb4e0e
heycn <x1chennan@qq.com>
2022-03-24 15:54:28 +0800

    useEffect 初体验

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
@@ -6,6 +6,27 @@ const App = () => {
   const onClick = () => {
     setN(i => i + 1)
   }
+
+  useEffect(() => {
+    console.log('第 1 次渲染执行')
+  }, [])
+
+  useEffect(() => {
+    console.log('任何一个 state 变化都会执行，包括第 1 次')
+  })
+
+  useEffect(() => {
+    console.log('每次 n 变化都会执行，包括第 1 次')
+  }, [n])
+
+  useEffect(() => {
+    if (n !== 0) {
+      console.log(`n 变化时执行，现在 n 是: ${n}`)
+    }
+  }, [n])
+
+  console.log('------ 分割线 ------')
+
   return (
     <div>
       n: {n}
@@ -14,4 +35,5 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = document.getElementById('root')
+ReactDOM.render(<App />, root)
