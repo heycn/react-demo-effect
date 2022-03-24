cf711f0ec80121e7e2d12452e2aac9a54933164f
heycn <x1chennan@qq.com>
2022-03-24 21:02:13 +0800

    使用 useLayoutEffect 来解决页面抖动

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const [n, setN] = useState(0)
-  const onClick = () => {
-    setN(i => i + 1)
-  }
+  const [value, setValue] = useState(0)
 
-  useEffect(() => {
-    const id = setInterval(() => {
-      console.log('hi')
-    }, 1000)
-    return () => {
-      window.clearInterval(id)
-    }
-  }, [])
+  // 这时网速慢的话，页面会从 value: 0 -> value: 1000
+  // useEffect(() => {
+  //   document.querySelector('#x').innerText = `value: 1000`
+  // }, [value])
+
+  // 这时候使用 useLayoutEffect 就可以解决 value 从 0 -> 1000 时抖动的情况
+  useLayoutEffect(() => {
+    document.querySelector('#x').innerText = `value: 1000`
+  }, [value])
 
   return (
-    <div>
-      n: {n}
-      <button onClick={onClick}>+1</button>
+    <div id='x' onClick={() => setValue(0)}>
+      value: {value}
     </div>
   )
 }
