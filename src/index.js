fb21e6bbe828a24a390af794bb143bef06056f53
heycn <x1chennan@qq.com>
2022-03-24 14:55:36 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [n, setN] = useState(0)
+  const onClick = () => {
+    setN(i => i + 1)
+  }
+  return (
+    <div>
+      n: {n}
+      <button onClick={onClick}>+1</button>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'))
